0e9d987fd10356a597e2c9ac9de8c3939de270da
David Verweij <davidverweij@gmail.com>
2020-08-13 16:04:05 +0200

    new: images lightbox

--- assets/scripts/project.js
+++ assets/scripts/project.js
@@ -52,3 +52,14 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var lightbox = new SimpleLightbox('.article_image a', {
+  animationSlide: false,
+  showCounter: false,
+  loop: true,
+  scaleImageToRatio: true,
+  disableRightClick: true,
+  disableScroll: true,
+  animationSpeed: 100,
+
+});
